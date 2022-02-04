28cd82bb0e3b1721b1e1be8545b0a4185bb8e727
syedahalam <syedahalam@yahoo.com>
2022-02-04 00:13:55 -0500

    sticky navbar done

--- script.js
+++ script.js
@@ -25,3 +25,16 @@ const countDown = () => {
 };
 // console.log(textSeconds);
 setInterval(countDown, 1000);
+
+// Sticky navbar////////////
+
+const nav = document.querySelector('.nav');
+window.addEventListener('scroll', fixNav);
+
+function fixNav() {
+	if (window.scrollY > nav.offsetHeight + 550) {
+		nav.classList.add('active');
+	} else {
+		nav.classList.remove('active');
+	}
+}
