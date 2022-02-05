f8fc5558232c4c19eef7a92c861a44ceae69bd57
syedahalam <syedahalam@yahoo.com>
2022-02-04 23:12:51 -0500

    done mobile nav

--- practice/script.js
+++ practice/script.js
@@ -1,27 +1,36 @@
 // timer////////////
-const countDown = () => {
-	const countDate = new Date('June 05, 2022 16:00:00').getTime();
-	// console.log(countDate);
-	const now = new Date().getTime();
+// const countDown = () => {
+// 	const countDate = new Date('June 05, 2022 16:00:00').getTime();
+// 	// console.log(countDate);
+// 	const now = new Date().getTime();
 
-	const gap = countDate - now;
+// 	const gap = countDate - now;
 
-	const seconds = 1000;
+// 	const seconds = 1000;
 
-	const minutes = seconds * 60;
-	const hours = minutes * 60;
-	const days = hours * 24;
+// 	const minutes = seconds * 60;
+// 	const hours = minutes * 60;
+// 	const days = hours * 24;
 
-	const textDays = Math.floor(gap / days);
-	const textHours = Math.floor((gap % days) / hours);
-	const textMinutes = Math.floor((gap % hours) / minutes);
-	const textSeconds = Math.floor((gap % minutes) / seconds);
+// 	const textDays = Math.floor(gap / days);
+// 	const textHours = Math.floor((gap % days) / hours);
+// 	const textMinutes = Math.floor((gap % hours) / minutes);
+// 	const textSeconds = Math.floor((gap % minutes) / seconds);
 
-	document.querySelector('.days').innerText = textDays;
-	document.querySelector('.hours').innerText = textHours;
-	document.querySelector('.minutes').innerText = textMinutes;
-	document.querySelector('.seconds').innerText = textSeconds;
-	// console.log(gap);
-};
-// console.log(textSeconds);
-setInterval(countDown, 1000);
+// 	document.querySelector('.days').innerText = textDays;
+// 	document.querySelector('.hours').innerText = textHours;
+// 	document.querySelector('.minutes').innerText = textMinutes;
+// 	document.querySelector('.seconds').innerText = textSeconds;
+// 	// console.log(gap);
+// };
+// // console.log(textSeconds);
+// setInterval(countDown, 1000);
+
+///////////////////////////////////////////Mobile Nav////////
+
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header-hero-img');
+
+btnNavEl.addEventListener('click', function () {
+	headerEl.classList.toggle('nav-open');
+});
